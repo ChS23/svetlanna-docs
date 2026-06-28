0deee94bccf14b5ca55c57ff9ce3d6d9a66e513b
Sergei Volchkov <63536056+ChS23@users.noreply.github.com>
2026-06-28 13:33:19 +0000

    Revert: unhide MNIST notebooks in sidebar

--- app/docs/course/optical-computing/_meta.js
+++ app/docs/course/optical-computing/_meta.js
@@ -1,6 +1,6 @@
 export default {
-  "02_mnist_mse_new.notebook": "Linear D2NN",
-  "4.notebook": "D2NN for MNIST Classification",
+  "02_mnist_mse_new.notebook": { display: "hidden" },
+  "4.notebook": { display: "hidden" },
   "5.notebook": "Diffractive Recurrent Neural Network (DRNN)",
   "6.notebook": "Diffractive Convolutional Neural Network (DCNN)"
 };
